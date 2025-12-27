af571d638138cee92f73553a887efb4d4e894ecd
Chiragcs8 <chirag98589@gmail.com>
2025-12-27 16:31:33 +0530

    Refactor todo item component and learning about directives

--- first-ng-app/src/app/todos/todos.component.ts
+++ first-ng-app/src/app/todos/todos.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodosService } from '../services/todos.service';
 import { Todo } from '../model/todo.type';
 import { catchError } from 'rxjs';
+import { TodoItemComponent } from '../components/todo-item/todo-item.component';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [],
+  imports: [TodoItemComponent],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss',
 })
@@ -15,13 +16,30 @@ export class TodosComponent implements OnInit {
   todoItems = signal<Array<Todo>>([]);
 
   ngOnInit(): void {
-    this.todoService.getTodosFromApi().pipe(
-      catchError((err) => {
-        console.log(err);
-        throw err
-      })
-    ).subscribe((todos) => {
-      this.todoItems.set(todos)
+    this.todoService
+      .getTodosFromApi()
+      .pipe(
+        catchError((err) => {
+          console.log(err);
+          throw err;
+        })
+      )
+      .subscribe((todos) => {
+        this.todoItems.set(todos);
+      });
+  }
+
+  updateTodoItem(todoItems: Todo) {
+    this.todoItems.update((todos) => {
+      return todos.map((todo) => {
+        if (todo.id === todoItems.id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      });
     });
   }
 }
