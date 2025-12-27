b888b009090aa429b6476e67de806663737215a2
Chiragcs8 <chirag98589@gmail.com>
2025-12-27 14:38:49 +0530

    Making simple counter to understand event and parsing functions

--- first-ng-app/src/app/home/home.component.ts
+++ first-ng-app/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { GreetingsComponent } from '../components/greetings/greetings.component';
+import { CounterComponent } from '../components/counter/counter.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [GreetingsComponent],
+  imports: [GreetingsComponent, CounterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
